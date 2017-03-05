457aa9b825ee95cdae7be4cd0742ba493e4834f2
pautzi <marcel.pautz@gmail.com>
2017-03-05 14:48:36 +0100

    Added IUOW and SO On

---
+++ FIT/Fit.Web/ViewModels/RequestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fit.Web.ViewModels
+{
+    public class RequestModel
+    {
+         public String Name { get; set; }
+    }
+}
