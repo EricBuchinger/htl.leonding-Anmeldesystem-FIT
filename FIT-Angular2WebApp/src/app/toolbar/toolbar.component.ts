7139c26b47e63d83e8c18fbcb9f2f918e5d186f3
PatrickMisti <patrickm99@hotmail.de>
2017-06-14 14:00:31 +0200

    Toolbar
    Erstellen der Toolbar

---
+++ FIT-Angular2WebApp/src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,47 @@
+import {Component,OnInit} from '@angular/core'
+
+@Component({
+    selector: '',
+    templateUrl: './toolbar.component.html',
+    styleUrls: ['./toolbar.component.css']
+})
+
+
+export class toolbar implements OnInit {
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+
+    all(){
+
+    }
+
+    confirmed(){
+
+    }
+
+    absEDVO(){
+
+    }
+
+    standOccupancy(){
+
+    }
+
+    workload(){
+
+    }
+
+    modification(){
+
+    }
+
+    media(){
+
+    }
+
+    administrativeData(){
+
+    }
+}
+
